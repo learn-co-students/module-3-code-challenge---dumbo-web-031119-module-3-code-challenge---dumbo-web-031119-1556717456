0be64cc788ebd110e3249beaf5064c037482e769
beschwa <naraiswar@gmail.com>
2019-05-01 10:41:30 -0400

    added createElement way

--- src/index.js
+++ src/index.js
@@ -2,32 +2,34 @@ const BEERURL = 'http://0.0.0.0:3000/beers'
 const beerUl = document.querySelector('#list-group')
 const beerDiv = document.querySelector('#beer-detail')
 
-
+// Main fetch
 fetch(BEERURL)
 .then((response) => {
 	return response.json()
 }).then((beers) => {
 	beers.forEach((beer) => {
-		beerUl.innerHTML += addBeerToUl(beer)
+		// beerUl.innerHTML += addBeerToUl(beer)  	//innerHTML way
+		addBeerToUlByCreatingElements(beer)			//createElement way
 	})
 })
 
+// Beer UL event listener
 beerUl.addEventListener('click', (event) => {
 	if(event.target.className === "list-group-item"){
 		getBeer(event.target.dataset.id).then((beer) => {
-			// debugger
-			beerDiv.innerHTML = showBeerInDiv(beer)
+			// beerDiv.innerHTML = showBeerInDiv(beer)	//innerHTML way
+			showBeerInDivByCreatingElements(beer)	//createElement way
 			addListener()
 		})
 	}
 })
 
-
-
+// innerHTML way
 const addBeerToUl = (beer) => {
 	return `<li class="list-group-item" data-id="${beer.id}">${beer.name}</li>`
 }
 
+// innerHTML way
 const showBeerInDiv = (beer) => {
  	return `<h1>${beer.name}</h1>
 	<img src="${beer.image_url}">
@@ -38,11 +40,46 @@ const showBeerInDiv = (beer) => {
 	</button>`
 }
 
+// createElement way - no return values
+const addBeerToUlByCreatingElements = (beer) => {
+	li = document.createElement('li')
+		li.className = "list-group-item"
+		li.dataset.id = `${beer.id}`
+		li.innerText = `${beer.name}`
+	beerUl.appendChild(li)
+}
+//createElement way - no return values
+const showBeerInDivByCreatingElements = (beer) => {
+
+	h1 = document.createElement('h1')
+		h1.innerText = `${beer.name}`
+	img = document.createElement('img')
+		img.src = `${beer.image_url}`
+	h3 = document.createElement('h3')
+		h3.innerText = `${beer.tagline}`
+	textarea = document.createElement('textarea')
+		textarea.innerText = `${beer.description}`
+	button = document.createElement('button')
+		button.id = "edit-beer"
+		button.className = "btn btn-info"
+		button.dataset.id = `${beer.id}`
+		button.innerText = "Save"
+
+	clearBeerDiv()
+	beerDiv.appendChild(h1)
+	beerDiv.appendChild(img)
+	beerDiv.appendChild(h3)
+	beerDiv.appendChild(textarea)
+	beerDiv.appendChild(button)						
+}
+
+// Get single beer
 const getBeer = (beerId) => {
 	return fetch(`${BEERURL}/${beerId}`)
 		.then(resp => resp.json())
 }
 
+// Adds listener to save description button in #beer-detail div
 const addListener = () => {
 	document.querySelector('#edit-beer').addEventListener('click', (event) => {
 		beerId = event.target.dataset.id
@@ -54,6 +91,7 @@ const addListener = () => {
 	})
 }
 
+// Updates description of specific beer
 const updateDescription = (beerId, newDescription) => {
 	return fetch(`${BEERURL}/${beerId}`, {
 		method: "PATCH",
@@ -65,4 +103,11 @@ const updateDescription = (beerId, newDescription) => {
 			"description": newDescription
 		})
 	}).then(resp => resp.json())
+}
+
+// Clear beer div for createElement way
+const clearBeerDiv = () => {
+	while(beerDiv.firstChild){
+		beerDiv.removeChild(beerDiv.firstChild)
+	}
 }
